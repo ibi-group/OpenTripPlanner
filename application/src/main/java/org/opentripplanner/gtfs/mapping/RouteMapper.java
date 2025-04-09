cdc170533194da9db2e8b0fe391fe6fb0c311696
Leonard Ehrenfried <mail@leonard.io>
2025-04-09 15:04:27 +0200

    Fix mapping of GroupOfRoutes

--- application/src/main/java/org/opentripplanner/gtfs/mapping/RouteMapper.java
+++ application/src/main/java/org/opentripplanner/gtfs/mapping/RouteMapper.java
@@ -82,7 +82,8 @@ class RouteMapper {
     lhs.withTextColor(rhs.getTextColor());
     lhs.withBikesAllowed(BikeAccessMapper.mapForRoute(rhs));
     if (rhs.getNetworkId() != null) {
-      var networkId = GroupOfRoutes.of(idFactory.createId(rhs.getNetworkId())).build();
+      var networkId = GroupOfRoutes.of(idFactory.createId(rhs.getNetworkId())
+      ).build();
       lhs.getGroupsOfRoutes().add(networkId);
     }
 
