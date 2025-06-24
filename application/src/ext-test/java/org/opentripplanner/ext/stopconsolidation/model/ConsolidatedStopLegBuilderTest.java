d6dd997a0f0450c32c4606f937b417e30e4f1c56
Leonard Ehrenfried <mail@leonard.io>
2025-06-24 13:54:56 +0200

    Take complex free transfers into account

--- application/src/ext-test/java/org/opentripplanner/ext/stopconsolidation/model/ConsolidatedStopLegBuilderTest.java
+++ application/src/ext-test/java/org/opentripplanner/ext/stopconsolidation/model/ConsolidatedStopLegBuilderTest.java
@@ -1,7 +1,7 @@
 package org.opentripplanner.ext.stopconsolidation.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.opentripplanner.ext.fares.impl._support.FareModelForTest.FARE_PRODUCT_USE;
+import static org.opentripplanner.ext.fares.impl._support.FareModelForTest.ANY_FARE_PRODUCT_USE;
 import static org.opentripplanner.transit.model._data.TimetableRepositoryForTest.id;
 
 import java.time.LocalDate;
@@ -36,7 +36,7 @@ class ConsolidatedStopLegBuilderTest implements PlanTestConstants {
       .withDistanceMeters(1000)
       .withAlightStopIndexInPattern(1)
       .build();
-  private static final List<FareProductUse> FARES = List.of(FARE_PRODUCT_USE);
+  private static final List<FareProductUse> FARES = List.of(ANY_FARE_PRODUCT_USE);
 
   @Test
   void build() {
